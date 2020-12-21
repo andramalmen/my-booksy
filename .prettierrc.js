e5b1bee0637afc9b00866be872a2b31e76abf335
Andra Malmen <andra.malmen@tieto.com>
2020-12-21 22:29:02 +0200

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    tabWidth: 4,
+    singleQuote: true,
+    semi: true,
+    trailingComma: 'es5',
+    printWidth: 100,
+    arrowParens: 'avoid',
+};
---
+++ android/app/src/main/java/com/mybooksy/MainActivity.java
@@ -0,0 +1,8 @@
+package com.mybooksy;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+
+  
+}
